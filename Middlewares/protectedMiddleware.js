617f4c2646305b1145cda7640409a9518f07afac
[ Ankit Fukte] <[ankitfukte11@gmail.com]>
2023-06-01 12:25:48 +0530

    added a feature to archive event when the event is over

--- Middlewares/protectedMiddleware.js
+++ Middlewares/protectedMiddleware.js
@@ -1,7 +1,68 @@
-export const protectedRoute = (req, res, next) => {
+import UserModel from "../Models/UserModel.js";
+
+export const protectedRoute = async (req, res, next) => {
   if (req.isAuthenticated()) {
-    return next();
+    const currentUserId = req.user._id;
+
+    const savedUserModel = await UserModel.findById(currentUserId);
+
+    if (!savedUserModel) {
+      return res.status(401).json({ status: false, message: "Unauthorized" });
+    }
+
+    if (savedUserModel.blocked) {
+      return res.status(401).json({
+        status: false,
+        message: "User is blocked cannot access this website",
+      });
+    } else {
+      return next();
+    }
   }
 
-  res.status(401).json({ error: "Unautorized user please login first" });
+  res.status(401).json({ error: "Unauthorized user please login first" });
+};
+
+export const protectedRouteOfTeamMember = async (req, res, next) => {
+  if (req.isAuthenticated()) {
+    const currentUserId = req.user._id;
+
+    const savedUser = await UserModel.findById(currentUserId);
+
+    if (!savedUser) {
+      return res.status(401).json({ status: false, message: "Unauthorized" });
+    }
+
+    if (savedUser.role === "Team-member") {
+      return next();
+    } else {
+      res.status(401).json({ error: "Only team-member can access this page" });
+    }
+  }
+
+  return res
+    .status(401)
+    .json({ status: false, message: "Unauthorized user please login first" });
+};
+
+export const protectedRouteOfAdmin = async (req, res, next) => {
+  if (req.isAuthenticated()) {
+    const currentUserId = req.user._id;
+
+    const savedUser = await UserModel.findById(currentUserId);
+
+    if (!savedUser) {
+      return res.status(401).json({ status: false, message: "Unauthorized" });
+    }
+
+    if (savedUser.role === "admin") {
+      return next();
+    } else {
+      res.status(401).json({ error: "Only team-member can access this page" });
+    }
+  }
+
+  return res
+    .status(401)
+    .json({ status: false, message: "Unauthorized user please login first" });
 };
