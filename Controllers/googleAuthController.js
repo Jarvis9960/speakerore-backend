28165d62a60e53bfa7b8f88689e11b4e2b4d278e
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-06-13 23:31:24 +0530

    adding redirect url

--- Controllers/googleAuthController.js
+++ Controllers/googleAuthController.js
@@ -11,7 +11,7 @@ export const authenticateCallback = passport.authenticate("google", {
 
 export const redirectCallback = (req, res) => {
   try {
-    res.redirect("http://localhost:3000");
+    res.redirect("https://lionfish-app-6d5du.ondigitalocean.app");
   } catch (error) {
     console.log(error);
   }
