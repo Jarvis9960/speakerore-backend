49aca82cc8d3ab9408dece4ff26bbe48d178f49e
[ Ankit Fukte] <[ankitfukte11@gmail.com]>
2023-06-14 11:33:52 +0530

    fixing bugs in production

--- Routes/speakeroreCouponRoute.js
+++ Routes/speakeroreCouponRoute.js
@@ -1,18 +1,31 @@
 import express from "express";
 const router = express.Router();
-import { createCoupon, getAllCoupon, applyCouponCode, getCouponBySearch, getCouponOfCurrentUserAffilate, createAffilateCoupon } from "../Controllers/speakerCouponController.js";
-import { protectedRoute, protectedRouteOfAdmin } from "../Middlewares/protectedMiddleware.js"
-
+import {
+  createCoupon,
+  getAllCoupon,
+  applyCouponCode,
+  getCouponBySearch,
+  getCouponOfCurrentUserAffilate,
+  createAffilateCoupon,
+} from "../Controllers/speakerCouponController.js";
+import {
+  protectedRoute,
+  protectedRouteOfAdmin,
+} from "../Middlewares/protectedMiddleware.js";
 
 router.post("/createcoupon", protectedRouteOfAdmin, createCoupon);
 
 router.get("/getallcoupons", protectedRouteOfAdmin, getAllCoupon);
 
-router.get("/applycouponcode", applyCouponCode);
+router.get("/applycouponcode", protectedRoute, applyCouponCode);
 
 router.get("/getcouponbyquery", protectedRouteOfAdmin, getCouponBySearch);
 
-router.get("/getaffilatecoupon", protectedRoute, getCouponOfCurrentUserAffilate);
+router.get(
+  "/getaffilatecoupon",
+  protectedRoute,
+  getCouponOfCurrentUserAffilate
+);
 
 router.post("/createaffilatecoupon", protectedRoute, createAffilateCoupon);
 
