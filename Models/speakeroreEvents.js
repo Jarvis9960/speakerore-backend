5a89480dcda90df8925a7a352fdb99a2ccdaa0cd
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-06-22 10:56:04 +0530

    changing pincode number validation to string

--- Models/speakeroreEvents.js
+++ Models/speakeroreEvents.js
@@ -86,7 +86,7 @@ const speakeroreEventSchema = new mongoose.Schema(
       required: true,
     },
     Pincode: {
-      type: Number,
+      type: String,
       required: true,
     },
     Country: {
