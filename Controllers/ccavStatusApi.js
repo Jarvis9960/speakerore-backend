9da35151e2df46c0b22c4604f8a252cc8f21512b
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-06-23 19:39:53 +0530

    Update ccavStatusApi.js

--- Controllers/ccavStatusApi.js
+++ Controllers/ccavStatusApi.js
@@ -9,6 +9,7 @@ export const postStatusApi = async (request, response) => {
     const referenceNo = request.params.reference_no || "";
     const access_code = "AVCF77KF59BD18FCDB";
     const params = { order_no: orderId, reference_no: referenceNo };
+    console.log(params)
     const paramsString = qs.stringify(params);
     console.log(paramsString);
     const encReq = encrypt(paramsString);
